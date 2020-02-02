a8c0f081067722f4564d3acaa3735ed2e90788f7
Jordan Trickett <jordan1trickett@gmail.com>
2020-02-03 09:16:25 +1100

    login functionality working

---
+++ app/controllers/auth_controller.rb
@@ -0,0 +1,35 @@
+class AuthController < ApplicationController
+
+  skip_before_action :verify_authenticity_token, raise: false
+  skip_before_action :require_login, only: [:login, :auto_login]
+
+  def login
+
+    user = User.find_by_email(params[:email])
+
+    if user && user.authenticate(params[:password])
+      payload = { user_id: user.id}
+      token = encode_token(payload)
+
+      render json: {user: user, jwt: token, success: "Welcome back, #{user.name}"}
+
+    else
+      render json: {failure: "Login failed! Invalid email or password"}
+    end
+
+  end
+
+
+  def auto_login
+    if session_user
+      render json: session_user
+    else
+      render json: {errors: "No user logged in"}
+    end
+  end
+
+  def user_is_authed
+    render json: {message: "You are authorised"}
+  end
+
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,26 @@
 class UsersController < ApplicationController
+  skip_before_action :require_login, only: [:create]
+
   def new
     @user = User.new
   end
 
   def create
+    @user = User.create user_params
+    if @user.valid?
+      payload = {user_id: @user.id}
+      token = encode_token(payload)
+      render json: {user: @user, jwt: token}
+    else
+      render json: {errors: user.errors.full_messages}, status: :not_acceptable
+    end
   end
 
   def index
     @users = User.all
   end
 
-  def show  
+  def show
     @user = User.find params[:id]
   end
 
@@ -21,5 +31,12 @@ class UsersController < ApplicationController
   end
 
   def destroy
+
+
   end
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
+
 end
