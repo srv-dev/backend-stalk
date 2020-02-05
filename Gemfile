7da6cb8e620ea6b7bfe20b7f2b264e11bed48e22
Sreevani Pasarlapati <sreevani.pasarla@gmail.com>
2020-02-05 16:34:14 +1100

    added knock gem

--- Gemfile
+++ Gemfile
@@ -57,3 +57,5 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem "rack-cors", "~> 1.1"
+
+gem "knock"
--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,6 @@ module BackendStalk
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    config.assets.initialize_on_precompile=true
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
